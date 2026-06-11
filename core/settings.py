8eace70404113069fcdde2621afcda850dbe0a97
chirag3084 <surtivatsal23@gmail.com>
2026-06-11 11:08:20 +0530

    remove cloudinary from installed apps

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "cloudinary_storage",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
